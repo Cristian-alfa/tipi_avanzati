2378d823dfcdfd6fb5bd48b415894863ca366b4f
Cristian-alfa <krisian.m01@gmail.com>
2019-01-05 12:08:54 +0100

    esercizio vettore

---
+++ sumvett.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int v[3];
+	int s = 0;
+	double m;
+
+	printf("Inserisci tre valori interi: \n");
+	scanf("%d", &v[0]);
+	scanf("%d", &v[1]);
+	scanf("%d", &v[2]);
+
+	printf("Dati iniziali: \n");
+	printf("numbers[0] = %d\n", v[0]);
+	printf("numbers[1] = %d\n", v[1]);
+	printf("numbers[2] = %d\n", v[2]);
+	
+	s = v[0] +v[1] +v[2];
+	m = s /3;
+
+	printf("Somma: %d\n", s);
+	printf("Media: %f\n", m);
+
+	return 0;
+}
